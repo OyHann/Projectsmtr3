dfc3709089a99fbe62641e3221690cfb970c5696
Muhammad Fariska Raihan <88713553+OyHann@users.noreply.github.com>
2024-12-17 17:03:04 +0700

    1 38 cuy

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client"
+import { setLoading } from '@/redux/features/loadingSlice';
+import axios from 'axios';
+import React, { useEffect } from 'react'
 
+export interface IProduct {
+  _id: string;
+  imgSrc: string;
+  fileKey: string;
+  name: string;
+  price: string;
+  category: string;
+}
 const Dashboard = () => {
+
+  const [products, setProducts] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [updateTable, setUpdateTable] = useState(false);
+
+const dispatch = useAppDispatch();
+
+
+useEffect(() => {
+  dispatch(setLoading(true));
+
+axios.
+get("/api/get_products")
+.then((res) => setProducts(res.data))
+.catch(err => console.log(err))
+.finally(() => dispatch(setLoading(false)));
+
+} [updateTable]);
+
   return (
     <div>Dashboard</div>
   )
