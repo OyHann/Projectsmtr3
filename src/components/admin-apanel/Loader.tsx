7d1cf644958ebd12db196be3d76b0314f4d8aea9
Muhammad Fariska Raihan <88713553+OyHann@users.noreply.github.com>
2024-12-18 20:34:59 +0700

    loader doang

--- src/components/admin-apanel/Loader.tsx
+++ src/components/admin-apanel/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
  const Loader = () => {
   return (
     <div className='fixed w-full h-screen top-0 left-0 bg-[#0000006d] grid place-items-center'>
-        <span className="Loader"></span>
+        <span className="loader"></span>
         </div>
   )
 };
