9b0e7b98dad1a30af21e473c8a367b3e88badd71
Muhammad Fariska Raihan <88713553+OyHann@users.noreply.github.com>
2024-12-25 23:59:54 +0700

    lebih 3 menit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Cart from '@/components/front-end/Cart';
+import Hero from '@/components/front-end/Hero';
 import Navbar from '@/components/front-end/Navbar';
 import React, { useState } from 'react';
 
@@ -10,7 +11,8 @@ const Home = () => {
   return (
     <main>
       <Navbar setShowCart={setShowCart} />
-      <Cart setShowCart={setShowCart} />
+      {showCart && <Cart setShowCart={setShowCart} />}
+      <Hero />
     </main>
   );
 };
